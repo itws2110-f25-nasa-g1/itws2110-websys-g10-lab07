7f772daf99732c0f405877c3b6ce09bcc43461eb
Spatialflunky1 <mjandrus06@gmail.com>
2025-11-07 15:03:48 -0500

    Updated lab7 database, including new tables and data

--- lab7.sql
+++ lab7.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 31, 2025 at 07:59 PM
+-- Generation Time: Nov 07, 2025 at 08:01 PM
 -- Server version: 11.8.3-MariaDB-0+deb13u1 from Debian
 -- PHP Version: 8.4.11
 
@@ -31,8 +31,49 @@ CREATE TABLE `courses` (
   `crn` int(11) NOT NULL,
   `prefix` varchar(4) NOT NULL,
   `number` smallint(4) NOT NULL,
-  `title` varchar(255) NOT NULL
+  `title` varchar(255) NOT NULL,
+  `section` int(4) NOT NULL,
+  `year` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`crn`, `prefix`, `number`, `title`, `section`, `year`) VALUES
+(35104, 'USNA', 2020, 'Seapower', 1, 2026),
+(36030, 'ARTS', 1200, 'Basic Drawing', 1, 2026),
+(36572, 'ITWS', 4500, 'Web Science Systems Development', 2, 2026),
+(38537, 'LITR', 6826, '200 Year Old Vampires Movie History', 1, 2026);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `grades`
+--
+
+CREATE TABLE `grades` (
+  `id` int(11) NOT NULL,
+  `crn` int(11) DEFAULT NULL,
+  `RIN` int(9) NOT NULL,
+  `grade` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Dumping data for table `grades`
+--
+
+INSERT INTO `grades` (`id`, `crn`, `RIN`, `grade`) VALUES
+(1, 36030, 3, 34),
+(2, 36572, 3, 47),
+(3, 38537, 3, 52),
+(4, 35104, 3, 63),
+(5, 36030, 1, 100),
+(6, 36572, 1, 101),
+(7, 38537, 1, 100),
+(8, 35104, 1, 99),
+(9, 36030, 4, 94),
+(10, 36572, 4, 98);
 
 -- --------------------------------------------------------
 
@@ -46,8 +87,22 @@ CREATE TABLE `students` (
   `first_name` varchar(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `alias` varchar(100) NOT NULL,
-  `phone` int(10) DEFAULT NULL
+  `phone` bigint(10) DEFAULT NULL,
+  `addr_street` varchar(64) NOT NULL,
+  `addr_city` varchar(64) NOT NULL,
+  `addr_state` varchar(64) NOT NULL,
+  `addr_zip` int(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`RIN`, `RCSID`, `first_name`, `last_name`, `alias`, `phone`, `addr_street`, `addr_city`, `addr_state`, `addr_zip`) VALUES
+(1, 'doej', 'Jane', 'Doe', 'jane', 5180000001, 'Street Ln', 'Troy', 'NY', 12180),
+(2, 'doej1', 'John', 'Doe', 'john', 5180000002, 'Road St', 'Troy', 'NY', 12180),
+(3, 'applej', 'Johnny', 'Appleseed', 'johnny', 5180000003, 'Boulevard Ave', 'Troy', 'NY', 12180),
+(4, 'lastna', 'Alex', 'Lastname', 'alex', 5180000004, 'Court Pkwy', 'Troy', 'NY', 12180);
 
 --
 -- Indexes for dumped tables
@@ -58,6 +113,14 @@ CREATE TABLE `students` (
 --
 ALTER TABLE `courses`
   ADD PRIMARY KEY (`crn`);
+
+--
+-- Indexes for table `grades`
+--
+ALTER TABLE `grades`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `crn` (`crn`),
+  ADD KEY `RIN` (`RIN`);
 
 --
 -- Indexes for table `students`
@@ -73,13 +136,30 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `crn` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `crn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38538;
+
+--
+-- AUTO_INCREMENT for table `grades`
+--
+ALTER TABLE `grades`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `RIN` int(9) NOT NULL AUTO_INCREMENT;
+  MODIFY `RIN` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `grades`
+--
+ALTER TABLE `grades`
+  ADD CONSTRAINT `foreign_key` FOREIGN KEY (`crn`) REFERENCES `courses` (`crn`),
+  ADD CONSTRAINT `foreign_key_2` FOREIGN KEY (`RIN`) REFERENCES `students` (`RIN`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
